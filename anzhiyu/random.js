d272be0aa76012a086cf764d5a0abcc4d0288e5e
yujiu312 <15938958525@163.com>
2026-01-09 14:19:49 +0800

    Site updated: 2026-01-09 14:19:48

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2026/01/07/evol主题配置/","2026/01/08/今日诗词/","2026/01/08/搜索/","2026/01/07/hexo主题配置/","2026/01/09/hexo/","2026/01/08/字数统计/","2026/01/07/永久链接/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
